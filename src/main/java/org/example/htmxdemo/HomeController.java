a51dda9fbbf1dc1c44c1b884e64351060f57aa3f
marit.van.dijk <mlvandijk@gmail.com>
2024-08-01 08:14:17 +0200

    Second demo

--- src/main/java/org/example/htmxdemo/HomeController.java
+++ src/main/java/org/example/htmxdemo/HomeController.java
@@ -6,12 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    static List<String> searchResults =
+            List.of("one", "two", "three", "four", "five");
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("results", searchResults);
         return "index";
+    }
+
+    @GetMapping("/search")
+    public String search(String q, Model model) {
+        var filtered = searchResults
+                .stream()
+                .filter(s -> s.startsWith(q.toLowerCase()))
+                .toList();
+
+        model.addAttribute("results", filtered);
+        return "search :: results";
     }
 
     @PostMapping("/clicked")
