02abedd629ea05e2331d89b7d068cded6d823bfe
marit.van.dijk <mlvandijk@gmail.com>
2024-08-01 08:08:28 +0200

    Initial commit

---
+++ src/main/java/org/example/htmxdemo/HtmxDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.htmxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmxDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/htmxdemo/HtmxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.htmxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
